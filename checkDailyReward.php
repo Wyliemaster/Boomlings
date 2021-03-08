e3636cc24339704fe9657985917e6a5300647c43
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-03-08 12:17:36 +0000

    fix daily reward

--- checkDailyReward.php
+++ checkDailyReward.php
@@ -3,7 +3,7 @@ include "incl/connection.php";
 include "incl/mainLib.php";
 
 $canClaim = 1;
-$today = date("yy-m-d");
+$today = date("y-m-d");
 
 $userID = MainLib::getUserIDOrDie($db, $_POST['udid']);
 
@@ -11,19 +11,17 @@ $query = $db->prepare("SELECT date FROM reward WHERE user_id = :userid ORDER BY 
 $query->execute(["userid" => $userID]);
 $dateArray = $query->fetchAll();
 
-
-if(!empty($dateArray) && $dateArray[0][0] == $today)
+if (!empty($dateArray) && $dateArray[0][0] == "20" . $today)
 	$canClaim = 0;
 else
 	array_unshift($dateArray, [$today]);
 
 $consecutiveDays = 0;
-do
-{
+do {
 	$lastDate = $dateArray[0][0];
 	array_shift($dateArray);
 	$consecutiveDays++;
-}while(!empty($dateArray) && $dateArray[0][0] == date("yy-m-d", strtotime($lastDate) - 86400));
+} while (!empty($dateArray) && $dateArray[0][0] == date("y-m-d", strtotime($lastDate) - 86400));
 
 $query = $db->prepare("INSERT INTO reward (user_id) VALUES (:userid)");
 $query->execute(["userid" => $userID]);

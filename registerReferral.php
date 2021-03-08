bf1d51e29040ef016daa0b04701c9cffb6b281b4
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-03-08 13:40:34 +0000

    improve registerReferral

--- registerReferral.php
+++ registerReferral.php
@@ -2,20 +2,36 @@
 include "incl/connection.php";
 include "incl/mainLib.php";
 
-if(!isset($_POST['udid']) || !isset($_POST['refID']))
+if (!isset($_POST['udid']) || !isset($_POST['refID']))
 	exit(-1);
 
-$referrerID = $db->prepare("SELECT id FROM user WHERE referral_code = :refID AND udid != :udid");
-$referrerID->execute([':refID' => $_POST['refID'], ':udid' => $_POST['udid']]);
-$referrerID = $referrerID->fetchColumn();
+$referrerID = $db->prepare("SELECT * FROM user WHERE referral_code = :refID");
+$referrerID->execute([':refID' => $_POST['refID']]);
+$referrerID = $referrerID->fetchAll();
+
+if (empty($referrerID[0][1])) {
+	exit("kE02");
+}
+
+if ($referrerID[0][1] == $_POST['udid']) {
+	exit("kE03");
+}
+
+$checkClaimed = $db->prepare("SELECT is_claimed FROM referral WHERE id=:id");
+$checkClaimed->execute([':id' => $referrerID[0][0]]);
+$checkClaimed = $checkClaimed->fetchColumn();
+
+if ($checkClaimed == 1) {
+	exit("kE01");
+}
 
 $refereeID = MainLib::getUserIDOrDie($db, $_POST['udid']);
 
-if(empty($referrerID) || empty($refereeID))
-	exit(-1);
+if (empty($referrerID) || empty($refereeID)) {
+	exit("kE02");
+}
 
-$refID = $db->prepare("INSERT INTO referral (referrer_id, referee_id) VALUES (:referrerID, :refereeID) ON DUPLICATE KEY UPDATE id = id");
-$refID->execute(['referrerID' => $referrerID, 'refereeID' => $refereeID]);
+$refID = $db->prepare("INSERT INTO referral (referrer_id, referee_id, is_claimed) VALUES (:referrerID, :refereeID, 1) ON DUPLICATE KEY UPDATE id = id");
+$refID->execute(['referrerID' => $referrerID[0][0], 'refereeID' => $refereeID]);
 
 echo 1;
-//TODO: duplicate referees
